748a996061929c42042e48a1ae02667dbc24a365
kdesnos <kdesnos@insa-rennes.fr>
2019-08-05 14:26:20 +0200

    (Project) Make coverage script for windows relative.

--- RunCoverageWindows.bat
+++ RunCoverageWindows.bat
@@ -1,4 +1,4 @@
-OpenCppCoverage.exe --source D:\Git-INSA\research\tpg\  --excluded_source  D:\Git-INSA\research\tpg\bin --export_type html:bin\coverage --excluded_line_regex "\s*else.*" --excluded_line_regex "\s*\{.*" .\bin\Debug\runTests.exe
+OpenCppCoverage.exe --source %cd% --excluded_source  %cd%\bin --export_type html:bin\coverage --excluded_line_regex "\s*else.*" --excluded_line_regex "\s*\{.*" .\bin\Debug\runTests.exe
 
 .\bin\coverage\index.html
 
