b0f96bdab993a6b2fa505c2ec0f4eda26aa578f7
kdesnos <kdesnos@insa-rennes.fr>
2019-10-30 15:13:01 +0100

    (Project) Update CPPCoverage script for windows.

--- RunCoverageWindows.bat
+++ RunCoverageWindows.bat
@@ -1,4 +1,4 @@
-OpenCppCoverage.exe --source %cd% --excluded_source  %cd%\bin --export_type html:bin\coverage --excluded_line_regex "\s*else.*" --excluded_line_regex "\s*\{.*" --excluded_line_regex "\s*\}.*" .\bin\Debug\runTests.exe
+OpenCppCoverage.exe --source %cd% --excluded_source  %cd%\bin --export_type html:bin\coverage --excluded_line_regex "\s*else.*" --excluded_line_regex "\s*\{.*" --excluded_line_regex "\s*\}.*" .\bin\bin\Debug\runTests.exe
 
 .\bin\coverage\index.html
 
