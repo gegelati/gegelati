f66c63bc1abbca35c5d558a9a9b47686af1ea772
kdesnos <kdesnos@insa-rennes.fr>
2019-10-30 17:36:49 +0100

    (Project) New gegelati.h header to ease inclusion of apps.

---
+++ gegelatilib/include/gegelati.h
@@ -0,0 +1,41 @@
+/**
+* \file gegelati.h
+* \brief Helper file gathering all headers from the GEGELATI lib to ease their 
+* inclusion in apps.
+*/
+#ifndef GEGELATI_H
+#define GEGELATI_H
+
+#include <dataHandlers/dataHandler.h>  
+#include <dataHandlers/primitiveTypeArray.h>
+
+#include <exporter/tpgGraphDotExporter.h>
+
+#include <instructions/addPrimitiveType.h>  
+#include <instructions/instruction.h>
+#include <instructions/lambdaInstruction.h>
+#include <instructions/multByConstParam.h>
+#include <instructions/set.h>
+
+#include <learn/learningAgent.h>
+#include <learn/learningEnvironment.h>
+#include <learn/learningParameters.h>
+
+#include <mutator/lineMutator.h>
+#include <mutator/mutationParameters.h>
+#include <mutator/programMutator.h>
+#include <mutator/rng.h>
+#include <mutator/tpgMutator.h>
+
+#include <program/line.h>  
+#include <program/program.h>  
+#include <program/programExecutionEngine.h>
+
+#include <tpg/tpgAction.h>
+#include <tpg/tpgEdge.h>
+#include <tpg/tpgExecutionEngine.h>
+#include <tpg/tpgGraph.h>
+#include <tpg/tpgTeam.h>
+#include <tpg/tpgVertex.h>
+
+#endif
