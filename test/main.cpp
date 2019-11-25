a8120b1f68f45f1c574861f6d3be45ae80b9cd21
kdesnos <kdesnos@insa-rennes.fr>
2019-11-25 12:36:46 +0100

    (Mutator) Attempt to import RNG from visual.

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,26 @@
 #include <gtest/gtest.h>
 
+#include "deterministicRandom.h"
+
+#include <iostream>
+#include <random>
+
 int main(int argc, char **argv) {
+
+	std::mt19937_64 engine(0);
+
+	Mutator::uniform_int_distribution<uint64_t> distribution(0, 100);
+
+	for (auto i = 0; i < 100; i++) {
+		std::cout << distribution(engine) << " " << std::endl;
+	}
+
+	Mutator::uniform_int_distribution<uint64_t> distribution2(0, UINT64_MAX);
+	for (auto i = 0; i < 100; i++) {
+		std::cout << distribution2(engine) << " " << std::endl;
+	}
+	
+
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
