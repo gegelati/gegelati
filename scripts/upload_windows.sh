ce11b72654870fb085c8a5c1f154a973346aad1f
kdesnos <kdesnos@insa-rennes.fr>
2019-12-09 16:45:28 +0100

    (CI) Deploy with transfer.sh

---
+++ scripts/upload_windows.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Uploads the built library onto transfer.sh
+PACKAGE_LOCATION="$(curl --upload-file ./bin/gegelatilib-0.0.0.zip https://transfer.sh/gegegelatilib-0.0.0.zip)"
+
+# Update url with short.cm
+curl --request POST --url https://api.short.cm/links/240039262 --header 'accept: application/json' --header 'authorization: nDpuKUpkjU6Zgpwc' --header 'content-type: application/json' --data '{"domain":"gegelati.shortcm.li","originalURL":"'$PACKAGE_LOCATION'","allowDuplicates":false}'
