1ea4bf52e111b038cd423a0d4a0b427e2bcd76d7
kdesnos <kdesnos@insa-rennes.fr>
2019-12-12 11:14:05 +0100

    (CI) HTML table for github pages.

--- scripts/download_archives_linux.sh
+++ scripts/download_archives_linux.sh
@@ -29,20 +29,39 @@ cd ${NEW_SITE_FOLDER}
 # Get latest archive from windows 
 curl -L -O -J https://gegelati.shortcm.li/windows-package
 
-# Prepare readme
+# Prepare index
+echo "# GEGELATI Neutral Builds 
+
+<table>
+<tr><td><b>Date</b></td><td><b>Time</b></td><td><b>Commit</b></td><td><b>Windows</b></td></tr>" > index.md  
+
+# And readme
 echo "# GEGELATI Neutral Builds 
 |Date|Time|Commit|Windows|
-|----|----|------|-------|" > index.md  
+|----|----|------|-------|" > ReadMe.md  
+
 
 # Generate list
 ARCHIVES=$(find -maxdepth 1 -regextype posix-egrep -regex "./gegelatilib-[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\.zip\$" | rev | cut -d'/' -f 1 | rev | sort -t. -k 1,1nr -k 2,2nr -k 3,3nr -k 4,4nr | head -n $MAX_VERSIONS_COUNT)
 
 count=0
 for archive in ${ARCHIVES}; do
-	echo "|$(git show -s --format="%ci" HEAD~$count| cut -d' ' -f 1 | cut -d'-' -f 1,2,3 --output-delimiter='.')|$(git show -s --format="%ci" HEAD~$count| cut -d' ' -f 2) |[$(git show -s --format="%h" HEAD~$count)](https://github.com/gegelati/gegelati/commit/$(git show -s --format="%H" HEAD~$count))|[Zip](./${archive})|" >> index.md 
+	# Variables
+	commit_date=$(git show -s --format="%ci" HEAD~$count| cut -d' ' -f 1 | cut -d'-' -f 1,2,3 --output-delimiter='.')
+	commit_time=$(git show -s --format="%ci" HEAD~$count| cut -d' ' -f 2)
+	commit_short_sha1=$(git show -s --format="%h" HEAD~$count)
+	commit_long_sha1=$(git show -s --format="%H" HEAD~$count)
+	archive_size=$(ls -s --block-size=K ./$archive | cut -d' ' -f 1)
+
+	# index.md html table
+	echo "<tr><td>$commit_date</td><td>$commit_time</td><td><a href=\"https://github.com/gegelati/gegelati/commit/$commit_long_sha1\"><code>$commit_short_sha1</code></a></td><td><a href=\"./$archive\">Zip ($archive_size)</a></td></tr>" >> index.md
+
+	# readme.md markdown table
+	echo "| $commit_date | $commit_time | [$commit_short_sha1](https://github.com/gegelati/gegelati/commit/$commit_long_sha1) | [Zip ($archive_size)](./${archive}) |" >> ReadMe.md 
 	count=$((count+1))
 done
 
+echo "</table>" >> index.md
 
 # Go back to parent
 cd ..  
