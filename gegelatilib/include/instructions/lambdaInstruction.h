4cde2150468c88473560142f035c78c620be4852
kdesnos <kdesnos@insa-rennes.fr>
2020-02-28 11:03:46 +0100

    (Instructions) Non-functional LambdaInstruction specialization for C-Style arrays.

--- gegelatilib/include/instructions/lambdaInstruction.h
+++ gegelatilib/include/instructions/lambdaInstruction.h
@@ -85,7 +85,7 @@ namespace Instructions {
 	* types derived from SupportedType.
 	*/
 	template <class T>
-	class LambdaInstruction <T, typename std::enable_if<std::is_base_of<Data::SupportedType, T>::value>::type> : public Instruction
+	class LambdaInstruction < T, typename std::enable_if <std::is_base_of<Data::SupportedType, T>::value>::type> : public Instruction
 	{
 	protected:
 		/**
@@ -122,6 +122,53 @@ namespace Instructions {
 			const T& arg1 = *std::dynamic_pointer_cast<const T>(args.at(0));
 			const T& arg2 = *std::dynamic_pointer_cast<const T>(args.at(1));
 			double result = this->func(arg1, arg2);
+			return result;
+		}
+	};
+
+	/**
+	* \brief Specialization of the LambdaInstruction template class for
+	* types derived from SupportedType and for array of type derived from
+	* SupportedType.
+	*/
+	template <class T>
+	class LambdaInstruction < T, typename std::enable_if <std::is_array<T>::value && std::is_base_of <Data::SupportedType, typename std::remove_all_extents<T>::type>::value>::type> : public Instruction
+	{
+	protected:
+		/**
+		* \brief Function executed for this Instruction.
+		*/
+		const std::function<double(const T, const T)> func;
+
+	public:
+		/**
+		* \brief delete the default constructor.
+		*/
+		LambdaInstruction() = delete;
+
+		/**
+		* \brief Constructor for the LambdaInstruction.
+		*
+		* \param[in] function the c++ std::function that will be executed for
+		* this Instruction.
+		*/
+		LambdaInstruction(std::function<double(const T, const T)> function) : func{ function } {
+			this->operandTypes.push_back(typeid(T));
+			this->operandTypes.push_back(typeid(T));
+		};
+
+		double execute(
+			const std::vector<std::reference_wrapper<const Parameter>>& params,
+			const std::vector<std::shared_ptr<const Data::SupportedType>>& args) const override {
+
+			// Check if operands and types have the right type.
+			if (Instruction::execute(params, args) != 1.0) {
+				return 0.0;
+			}
+
+			std::shared_ptr<const T> arg1 = std::dynamic_pointer_cast<const T>(args.at(0));
+			std::shared_ptr<const T> arg2 = std::dynamic_pointer_cast<const T>(args.at(1));
+			double result = this->func(arg1.get(), arg2.get());
 			return result;
 		}
 	};
