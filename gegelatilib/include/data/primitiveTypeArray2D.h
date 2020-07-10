09f96664d668c8f658e4ee54aa67674a7f6931c1
kdesnos <kdesnos@insa-rennes.fr>
2020-07-10 13:09:27 +0200

    (Data) Implement PrimitiveTypeArray2D::getAddressSpace() method.

--- gegelatilib/include/data/primitiveTypeArray2D.h
+++ gegelatilib/include/data/primitiveTypeArray2D.h
@@ -33,13 +33,49 @@ namespace Data {
          * \param[in] h The height of the 2D array.
          */
         PrimitiveTypeArray2D(const size_t w = 2, const size_t h = 4);
+
+        // Inherited from DataHandler
+        virtual size_t getAddressSpace(
+            const std::type_info& type) const override;
     };
 
     template <typename T>
     inline PrimitiveTypeArray2D<T>::PrimitiveTypeArray2D(const size_t w,
                                                          const size_t h)
-        : PrimitiveTypeArray<T>(h * w)
+        : PrimitiveTypeArray<T>(h * w), height(h), width(w)
     {
     }
+
+    template <typename T>
+    inline size_t PrimitiveTypeArray2D<T>::getAddressSpace(
+        const std::type_info& type) const
+    {
+        // Check if type is provided by underlying 1D array.
+        size_t result = Data::PrimitiveTypeArray<T>::getAddressSpace(type);
+
+        // If the requested type is not provided by the undetlying 1D array
+        // Check if it is a 2D array
+        if (result == 0) {
+
+            // If the type is a 2D array of the primitive type
+            // with a size (h*w) inferior to the container dimensions.
+            std::string typeName = DEMANGLE_TYPEID_NAME(type.name());
+            std::string regex{DEMANGLE_TYPEID_NAME(typeid(T).name())};
+            regex.append("\\s*(const\\s*)?\\[([0-9]+)\\]\\[([0-9]+)\\]");
+            std::regex arrayType(regex);
+            std::cmatch cm;
+            if (std::regex_match(typeName.c_str(), cm, arrayType)) {
+                int height = std::atoi(cm[2].str().c_str());
+                int width = std::atoi(cm[3].str().c_str());
+                if (height <= this->height && width <= this->width) {
+                    return (this->height - height + 1) *
+                           ((this->width - width + 1));
+                }
+            }
+        }
+
+        return result;
+    }
+
 }; // namespace Data
 #endif
--- test/primitiveTypeArray2DTest.cpp
+++ test/primitiveTypeArray2DTest.cpp
@@ -2,10 +2,45 @@
 
 #include "data/primitiveTypeArray2D.h"
 
-TEST(PrimitiveTypeArrayTest, Constructor)
+TEST(PrimitiveTypeArray2DTest, Constructor)
 {
     Data::PrimitiveTypeArray2D<double>* array;
     ASSERT_NE(array = new Data::PrimitiveTypeArray2D<double>(3, 4), nullptr)
         << "A PrimitiveTypeArray2D<double> could not be built successfully.";
-    ASSERT_NO_THROW(delete array) << "PrimitiveTypeArray2D could not be deleted.";
+    ASSERT_NO_THROW(delete array)
+        << "PrimitiveTypeArray2D could not be deleted.";
+}
+
+TEST(PrimitiveTypeArray2DTest, getAddressSpace)
+{
+    size_t h = 3;
+    size_t w = 5;
+    Data::PrimitiveTypeArray2D<int> a(w, h);
+
+    // Check primitive type provided by 1D array
+    // (other types are covered by PrimitiveTypeArray tests)
+    ASSERT_EQ(a.getAddressSpace(typeid(int)), w * h)
+        << "Address space of the 2D array of int is not width*height for "
+           "typeid(int).";
+
+    // Request a 2D array with valid dimensions
+    ASSERT_EQ(a.getAddressSpace(typeid(int[2][4])), (w - 4 + 1) * (h - 2 + 1))
+        << "Returned address space for int[2][4] in a 2D int array of size 5x3 "
+           "is incorrect.";
+
+    // Request a const 2D array with valid dimensions
+    ASSERT_EQ(a.getAddressSpace(typeid(const int[2][4])),
+              (w - 4 + 1) * (h - 2 + 1))
+        << "Returned address space for int[2][4] in a 2D int array of size 5x3 "
+           "is incorrect.";
+
+    // Request a 2D array with invalid dimensions
+    ASSERT_EQ(a.getAddressSpace(typeid(int[4][2])), 0)
+        << "Returned address space for int[4][2] in a 2D int array of size 5x3 "
+           "is incorrect.";
+
+    // Request a 2D array with invalid type
+    ASSERT_EQ(a.getAddressSpace(typeid(long[1][1])), 0)
+        << "Returned address space for int[4][2] in a 2D int array of size 5x3 "
+           "is incorrect.";
 }
