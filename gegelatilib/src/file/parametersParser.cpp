04c9b5d33421a24ca77326196e4e2d9f154418f8
kdesnos <kdesnos@insa-rennes.fr>
2021-02-26 11:28:12 +0100

    (Learn|File) Add comments to parameters in JSon file. All parameters now also have a default value.

--- gegelatilib/src/file/parametersParser.cpp
+++ gegelatilib/src/file/parametersParser.cpp
@@ -238,61 +238,166 @@ void File::ParametersParser::loadParametersFromJson(
 void File::ParametersParser::writeParametersToJson(
     const char* path, const Learn::LearningParameters& params)
 {
+    Learn::LearningParameters defaultParams;
     // Create the JSON Structure
     Json::Value root;
 
     // Fill it
     // base parameters
     root["archiveSize"] = params.archiveSize;
+    root["archiveSize"].setComment(
+        Learn::LearningParameters::archiveSizeComment, Json::commentBefore);
+
     root["archivingProbability"] = params.archivingProbability;
+    root["archivingProbability"].setComment(
+        Learn::LearningParameters::archivingProbabilityComment,
+        Json::commentBefore);
+
     root["doValidation"] = params.doValidation;
+    root["doValidation"].setComment(
+        Learn::LearningParameters::doValidationComment, Json::commentBefore);
+
     root["maxNbActionsPerEval"] = params.maxNbActionsPerEval;
+    root["maxNbActionsPerEval"].setComment(
+        Learn::LearningParameters::maxNbActionsPerEvalComment,
+        Json::commentBefore);
+
     root["maxNbEvaluationPerPolicy"] = params.maxNbEvaluationPerPolicy;
+    root["maxNbEvaluationPerPolicy"].setComment(
+        Learn::LearningParameters::maxNbEvaluationPerPolicyComment,
+        Json::commentBefore);
+
     root["nbGenerations"] = params.nbGenerations;
+    root["nbGenerations"].setComment(
+        Learn::LearningParameters::nbGenerationsComment, Json::commentBefore);
+
     root["nbIterationsPerJob"] = params.nbIterationsPerJob;
+    root["nbIterationsPerJob"].setComment(
+        Learn::LearningParameters::nbIterationsPerJobComment,
+        Json::commentBefore);
+
     root["nbIterationsPerPolicyEvaluation"] =
         params.nbIterationsPerPolicyEvaluation;
+    root["nbIterationsPerPolicyEvaluation"].setComment(
+        Learn::LearningParameters::nbIterationsPerPolicyEvaluationComment,
+        Json::commentBefore);
+
     root["nbProgramConstant"] = params.nbProgramConstant;
+    root["nbProgramConstant"].setComment(
+        Learn::LearningParameters::nbProgramConstantComment,
+        Json::commentBefore);
+
     root["nbRegisters"] = params.nbRegisters;
+    root["nbRegisters"].setComment(
+        Learn::LearningParameters::nbRegistersComment, Json::commentBefore);
+
     root["nbThreads"] = params.nbThreads;
+    root["nbThreads"].setComment(Learn::LearningParameters::nbThreadsComment,
+                                 Json::commentBefore);
+
     root["ratioDeletedRoots"] = params.ratioDeletedRoots;
+    root["ratioDeletedRoots"].setComment(
+        Learn::LearningParameters::ratioDeletedRootsComment,
+        Json::commentBefore);
 
     // Mutation.tpg parameters
     root["mutation"]["tpg"]["forceProgramBehaviorChangeOnMutation"] =
         params.mutation.tpg.forceProgramBehaviorChangeOnMutation;
+    root["mutation"]["tpg"]["forceProgramBehaviorChangeOnMutation"].setComment(
+        Mutator::TPGParameters::forceProgramBehaviorChangeOnMutationComment,
+        Json::commentBefore);
+
     root["mutation"]["tpg"]["maxInitOutgoingEdges"] =
         params.mutation.tpg.maxInitOutgoingEdges;
+    root["mutation"]["tpg"]["maxInitOutgoingEdges"].setComment(
+        Mutator::TPGParameters::maxInitOutgoingEdgesComment,
+        Json::commentBefore);
+
     root["mutation"]["tpg"]["maxOutgoingEdges"] =
         params.mutation.tpg.maxOutgoingEdges;
+    root["mutation"]["tpg"]["maxOutgoingEdges"].setComment(
+        Mutator::TPGParameters::maxOutgoingEdgesComment, Json::commentBefore);
+
     root["mutation"]["tpg"]["nbActions"] = params.mutation.tpg.nbActions;
+    root["mutation"]["tpg"]["nbActions"].setComment(
+        Mutator::TPGParameters::nbActionsComment, Json::commentBefore);
+
     root["mutation"]["tpg"]["nbRoots"] = params.mutation.tpg.nbRoots;
+    root["mutation"]["tpg"]["nbRoots"].setComment(
+        Mutator::TPGParameters::nbRootsComment, Json::commentBefore);
+
     root["mutation"]["tpg"]["pEdgeAddition"] =
         params.mutation.tpg.pEdgeAddition;
+    root["mutation"]["tpg"]["pEdgeAddition"].setComment(
+        Mutator::TPGParameters::pEdgeAdditionComment, Json::commentBefore);
+
     root["mutation"]["tpg"]["pEdgeDeletion"] =
         params.mutation.tpg.pEdgeDeletion;
+    root["mutation"]["tpg"]["pEdgeDeletion"].setComment(
+        Mutator::TPGParameters::pEdgeDeletionComment, Json::commentBefore);
+
     root["mutation"]["tpg"]["pEdgeDestinationChange"] =
         params.mutation.tpg.pEdgeDestinationChange;
+    root["mutation"]["tpg"]["pEdgeDestinationChange"].setComment(
+        Mutator::TPGParameters::pEdgeDestinationChangeComment,
+        Json::commentBefore);
+
     root["mutation"]["tpg"]["pEdgeDestinationIsAction"] =
         params.mutation.tpg.pEdgeDestinationIsAction;
+    root["mutation"]["tpg"]["pEdgeDestinationIsAction"].setComment(
+        Mutator::TPGParameters::pEdgeDestinationIsActionComment,
+        Json::commentBefore);
+
     root["mutation"]["tpg"]["pProgramMutation"] =
         params.mutation.tpg.pProgramMutation;
+    root["mutation"]["tpg"]["pProgramMutation"].setComment(
+        Mutator::TPGParameters::pProgramMutationComment, Json::commentBefore);
 
     // Mutation.program parameters
     root["mutation"]["prog"]["maxConstValue"] =
         params.mutation.prog.maxConstValue;
+    root["mutation"]["prog"]["maxConstValue"].setComment(
+        Mutator::ProgramParameters::maxConstValueComment, Json::commentBefore);
+
     root["mutation"]["prog"]["maxProgramSize"] =
         params.mutation.prog.maxProgramSize;
+    root["mutation"]["prog"]["maxProgramSize"].setComment(
+        Mutator::ProgramParameters::maxProgramSizeComment, Json::commentBefore);
+
     root["mutation"]["prog"]["minConstValue"] =
         params.mutation.prog.minConstValue;
+    root["mutation"]["prog"]["minConstValue"].setComment(
+        Mutator::ProgramParameters::minConstValueComment, Json::commentBefore);
+
     root["mutation"]["prog"]["pAdd"] = params.mutation.prog.pAdd;
+    root["mutation"]["prog"]["pAdd"].setComment(
+        Mutator::ProgramParameters::pAddComment, Json::commentBefore);
+
     root["mutation"]["prog"]["pConstantMutation"] =
         params.mutation.prog.pConstantMutation;
+    root["mutation"]["prog"]["pConstantMutation"].setComment(
+        Mutator::ProgramParameters::pConstantMutationComment,
+        Json::commentBefore);
+
     root["mutation"]["prog"]["pDelete"] = params.mutation.prog.pDelete;
+    root["mutation"]["prog"]["pDelete"].setComment(
+        Mutator::ProgramParameters::pDeleteComment, Json::commentBefore);
+
     root["mutation"]["prog"]["pMutate"] = params.mutation.prog.pMutate;
+    root["mutation"]["prog"]["pMutate"].setComment(
+        Mutator::ProgramParameters::pMutateComment, Json::commentBefore);
+
     root["mutation"]["prog"]["pSwap"] = params.mutation.prog.pSwap;
+    root["mutation"]["prog"]["pSwap"].setComment(
+        Mutator::ProgramParameters::pSwapComment, Json::commentBefore);
 
     // Write to the output stream
     std::ofstream writtenFile(path);
-    writtenFile << root;
+    Json::StreamWriterBuilder writerFactory;
+    // Set a precision to 6 digits after the point.
+    writerFactory.settings_["precision"] = 6U;
+    Json::StreamWriter* writer = writerFactory.newStreamWriter();
+    writer->write(root, &writtenFile);
+    delete writer;
     writtenFile.close();
 }
