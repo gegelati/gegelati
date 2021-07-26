85cca9fa4d1635c800555a1dffe7d54f3c1c0180
Bourgoin Thomas <thomas.bourgoin@insa-rennes.fr>
2021-07-26 13:22:32 +0200

    (Test) Fix path in variable build and use it instead of duplicating its value

--- test/dat/codeGen/compile.bat
+++ test/dat/codeGen/compile.bat
@@ -2,11 +2,11 @@ set DIR=%1
 set DAT=%2
 set target=%3
 
-set build=%DIR%/buildCodeGen/%target%
+set build=%DIR%/build/CodeGen/%target%
 
 if not exist %build% mkdir %build%
 
-pushd %DIR%/build/CodeGen/%target%
+pushd %build%
 
 cmake -DDIR=%DIR% %DAT%codeGen/%target%
 
