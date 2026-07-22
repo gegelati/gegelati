bed0bbac05eb512f8acab3ff9668a3f9c8ae155f
kdesnos <kdesnos@insa-rennes.fr>
2026-07-22 12:59:24 +0200

    (RelEng) Add a sandbox project to the hierarchy. This should be used to test and debug library code with a custom app without having to create a separate project.
    Signed-off-by: kdesnos <kdesnos@insa-rennes.fr>

---
+++ sandbox/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file main.cpp
+ * @author Karol Desnos
+ * @brief Main file for the sandbox application enabling to test the framework
+ * directly without the need to create a new project.
+ */
+#include <iostream>
+
+#include "gegelati.h"
+
+int main(int argc, char** argv)
+{
+    std::cout << "Gegelati Sandbox" << std::endl;
+}
