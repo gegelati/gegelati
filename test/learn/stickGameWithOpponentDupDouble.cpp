17eba16879bed57f39c915284b8ecf1ac2860523
Quentin Vacher <quentinvcher@gmail.com>
2026-08-19 14:23:55 +0200

    (typo) case issue in import

--- test/learn/stickGameWithOpponentDupDouble.cpp
+++ test/learn/stickGameWithOpponentDupDouble.cpp
@@ -34,7 +34,7 @@
  * knowledge of the CeCILL-C license and that you accept its terms.
  */
 
-#include "StickGameWithOpponentDupDouble.h"
+#include "stickGameWithOpponentDupDouble.h"
 
 bool StickGameWithOpponentD::isCopyable() const
 {
